0a1f425e9ce358b7a0fc3d052f882a39de2da3fc
mauri0098 <maurirodriguez0098@gmail.com>
2024-04-09 22:23:55 -0300

    Agregar archivos de proyecto.

---
+++ ClaseNave.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace pryJuegos
+{
+    internal class ClaseNave
+    {
+        //Caracteristicas
+
+        public int vida;
+        public string nombre;
+         int puntosDeDaños;
+        public PictureBox imgNave;
+        public void Crearjuegador()
+        {
+            vida = 100;
+            nombre = "Jugador1";
+            puntosDeDaños = 1;
+            imgNave = new PictureBox();
+            imgNave.SizeMode = PictureBoxSizeMode.StretchImage;
+            imgNave.ImageLocation = "https://cdna.artstation.com/p/assets/images/images/017/509/756/large/rutger-slopsema-galaga.jpg?1556264895";  
+        }
+        public void CrearEnemigo()
+        {
+
+            vida = 30;
+            nombre = "Enemigo1";
+            puntosDeDaños = 1;
+
+        }
+
+
+
+    }
+
+}
---
+++ frmJuegos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryJuegos
+{
+    public partial class frmJuegos : Form
+    {
+        public frmJuegos()
+        {
+            InitializeComponent();
+        }
+        ClaseNave ObjNavejuegador;
+        private void frmJuegos_Load(object sender, EventArgs e)
+        {
+            ObjNavejuegador = new ClaseNave();
+            ObjNavejuegador.Crearjuegador();
+
+            Controls.Add(ObjNavejuegador.imgNave);
+
+            ObjNavejuegador.imgNave.Location = new Point(500, 500);
+
+        }
+
+        private void frmJuegos_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { 
+                
+                ObjNavejuegador.imgNave.Location = new Point(ObjNavejuegador.imgNave.Location.X+5, 
+                ObjNavejuegador.imgNave.Location.Y);   
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+
+                ObjNavejuegador.imgNave.Location = new Point(ObjNavejuegador.imgNave.Location.X - 5,
+                ObjNavejuegador.imgNave.Location.Y);
+            }
+        }
+    }
+}
