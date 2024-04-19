8651365ec4242e06a11c98d578243d7307261335
mauri0098 <maurirodriguez0098@gmail.com>
2024-04-19 07:49:12 -0300

    2005

--- frmJuegos.Designer.cs
+++ frmJuegos.Designer.cs
@@ -42,7 +42,7 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources._0_F7Qnr7Zs_dobles_s___1_;
-            pictureBox1.Location = new Point(260, 341);
+            pictureBox1.Location = new Point(12, 57);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(100, 50);
             pictureBox1.TabIndex = 0;
